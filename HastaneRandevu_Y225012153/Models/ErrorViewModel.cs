26299124cb4d8033f4e631b48c644b622990a945
unknown <ozkan.aysenur@hotmail.com>
2023-12-20 16:44:52 +0300

    WebProgramlamaProje

---
+++ HastaneRandevu_Y225012153/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HastaneRandevu_Y225012153.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

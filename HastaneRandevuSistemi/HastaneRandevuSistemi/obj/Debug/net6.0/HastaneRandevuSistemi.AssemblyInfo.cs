58854a38a045a9c2f999f2c68de4b2f905e5a808
unknown <ozkan.aysenur@hotmail.com>
2024-01-01 19:35:22 +0300

    WebProgramlamaProje

--- HastaneRandevuSistemi/HastaneRandevuSistemi/obj/Debug/net6.0/HastaneRandevuSistemi.AssemblyInfo.cs
+++ HastaneRandevuSistemi/HastaneRandevuSistemi/obj/Debug/net6.0/HastaneRandevuSistemi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HastaneRandevuSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2832ad084f6f64cf80a4b2fbf10c4799eeee827")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6cedefb9a38129439eb16a2dc745c152d24fd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HastaneRandevuSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HastaneRandevuSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

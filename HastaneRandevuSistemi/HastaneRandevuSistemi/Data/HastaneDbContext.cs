a2832ad084f6f64cf80a4b2fbf10c4799eeee827
aysenurozkann <66972339+aysenurozkann@users.noreply.github.com>
2024-01-01 16:31:03 +0300

    Update HastaneDbContext.cs

--- HastaneRandevuSistemi/HastaneRandevuSistemi/Data/HastaneDbContext.cs
+++ HastaneRandevuSistemi/HastaneRandevuSistemi/Data/HastaneDbContext.cs
@@ -9,12 +9,11 @@ namespace HastaneRandevuSistemi.Data
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;" +
-                "Database=HastaneDb;Trusted_Connection=True;MultipleActiveResultSets=true;"); // parantez içine ; koydum, hata veriyorsa kontrol et
-        }
+                "Database=HastaneDb;Trusted_Connection=True;MultipleActiveResultSets=true;");
         public DbSet<AnaBilimDali> AnaBilimDallari { get; set; }
-        public DbSet<Kullanicilar> Kullanicilar { get; set; } // burası hata verecek mi kontrol et, aynı isimde
+        public DbSet<Kullanicilar> Kullanicilar { get; set; } 
         public DbSet<Doktor> Doktorlar { get; set; }
-        public DbSet<Poliklinikler> Poliklinikler { get; set; } // burası hata verecek mi kontrol et, aynı isimde
+        public DbSet<Poliklinikler> Poliklinikler { get; set; } 
         public DbSet<Randevu> Randevular { get; set; }
         public DbSet<CalismaSaatleri> CalismaSaatleri { get; set; }
 
